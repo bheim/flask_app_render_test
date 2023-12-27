c7cc332473dbcc30b257b29b567a7496cca40c84
bheim <109227608+bheim@users.noreply.github.com>
2023-12-27 10:27:06 -0500

    more requirements

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
 import pandas as pd
 import numpy as np
+from gensim.models.word2vec import Word2Vec
+import gensim.downloader as api
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+
+with open('mt_predict.pkl', 'rb') as file:
+    mt_predict = pickle.load(file)
+
+corpus = api.load('text8')
+model = Word2Vec(corpus)
+
+
+vocab = list(model.wv.index_to_key)
 
 app = Flask(__name__)
 
